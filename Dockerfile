d64b35e5698d411de1650f166c4ef05540854c69
jonycodes <mr.joannier@yahoo.com>
2017-02-15 13:37:20 -0500

    added mongo db image and persistent database

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:2.7.10
 WORKDIR /reckonfiu
 ADD . /reckonfiu
 RUN pip install -r requirements.txt
-CMD ["python", "app/server.py"]
+CMD ["python", "-u" ,"app/server.py"]
--- app/server.py
+++ app/server.py
@@ -1,11 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient(
+    'db', 
+    27017)
+
+db = client.fiudb
 
 @app.route('/')
 def hello():
     return "Hello World"
 
+@app.route('/getall')
+def allRecords():
+    cursor = db.courses.find()
+    results = []
+    for document in cursor:
+        results.append(document['course'])
+    return jsonify({ 'count': cursor.count() , 'results': results})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
+    
